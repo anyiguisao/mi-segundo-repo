e6e263db2bdb653ff3455e89df2026adbeb2a948
anyiguisao <guisaocardonaanyimarcela@gamil.com>
2022-02-15 09:09:24 -0500

    mi segindo repo

---
+++ segundorepo.py
@@ -0,0 +1,8 @@
+Acumulado=0 
+for i in range (11):
+    numero = int(input("Ingrese su numero: "))
+    Acumulado = Acumulado + numero 
+
+
+nota = Acumulado  / 10 
+print(f"el promedido de los 10 numeros es {nota}")
